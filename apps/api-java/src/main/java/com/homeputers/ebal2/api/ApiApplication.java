f4daa67146f430eadd1cd0c1c4e4337589e4893c
Andres Rovira <wrovira@paypal.com>
2025-09-12 18:13:22 -0600

    chore(api-java): remove wrapper jar and stray app

--- apps/api-java/src/main/java/com/homeputers/ebal2/api/ApiApplication.java
+++ apps/api-java/src/main/java/com/homeputers/ebal2/api/ApiApplication.java
@@ -1,11 +1,11 @@
-package com.ebal.api;
+package com.homeputers.ebal2.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiJavaApplication {
+public class ApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiJavaApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 }
