c7e25437e9796e51a491e68ce3f657a45aea145c
Andres Rovira <wrovira@paypal.com>
2025-09-12 21:10:35 -0600

    fix(api): split controllers by tag

--- apps/api-java/src/main/java/com/homeputers/ebal2/api/HealthController.java
+++ apps/api-java/src/main/java/com/homeputers/ebal2/api/HealthController.java
@@ -1,12 +1,12 @@
 package com.homeputers.ebal2.api;
 
-import com.homeputers.ebal2.api.generated.SystemApi;
+import com.homeputers.ebal2.api.generated.HealthApi;
 import com.homeputers.ebal2.api.generated.model.Health;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthController implements SystemApi {
+public class HealthController implements HealthApi {
 
     @Override
     public ResponseEntity<Health> health() {
