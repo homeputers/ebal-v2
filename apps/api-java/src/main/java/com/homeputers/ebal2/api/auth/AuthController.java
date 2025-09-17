22e1b0e27206e644f9916e0d2972d85d21eb0cdd
Andres Rovira <wrovira@paypal.com>
2025-09-17 12:09:19 -0600

    refactor(api): implement auth api current user

--- apps/api-java/src/main/java/com/homeputers/ebal2/api/auth/AuthController.java
+++ apps/api-java/src/main/java/com/homeputers/ebal2/api/auth/AuthController.java
@@ -1,6 +1,6 @@
 package com.homeputers.ebal2.api.auth;
 
-import com.homeputers.ebal2.api.generated.MeApi;
+import com.homeputers.ebal2.api.generated.AuthApi;
 import com.homeputers.ebal2.api.generated.model.CurrentUser;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -10,11 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
-public class MeController implements MeApi {
+public class AuthController implements AuthApi {
 
     private final CurrentUserFactory currentUserFactory;
 
-    public MeController(CurrentUserFactory currentUserFactory) {
+    public AuthController(CurrentUserFactory currentUserFactory) {
         this.currentUserFactory = currentUserFactory;
     }
 
--- apps/api-java/src/test/java/com/homeputers/ebal2/api/auth/AuthControllerTest.java
+++ apps/api-java/src/test/java/com/homeputers/ebal2/api/auth/AuthControllerTest.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class MeControllerTest extends AbstractIntegrationTest {
+class AuthControllerTest extends AbstractIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
